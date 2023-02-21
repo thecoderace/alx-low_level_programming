0bcbf223bc8158996b583ae5d3104fcf57818afd
Kara Jacobs <jacobskara@outlook.com>
2023-02-21 09:31:24 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,16 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n *= -1;
+	int last;
 
-       	_putchar('0' + (n % 10));
+	last = n % 10;
 
-	return (n % 10);
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+
+	_putchar(last + '0');
+
+	return (last);
 }
