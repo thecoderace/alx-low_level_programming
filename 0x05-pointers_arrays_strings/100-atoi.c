1df1fcbe2eb8e7ab12943da4144155834e8ec5d9
Kara Jacobs <jacobskara@outlook.com>
2023-02-27 09:44:54 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,11 +6,13 @@
  *
  * Return: the integer value of the input string
  */
+
 int _atoi(char *s)
 {
     int i = 0;
     int sign = 1;
     int result = 0;
+    int limit = INT_MAX / 10;
 
     while (s[i] != '\0')
     {
@@ -20,6 +22,13 @@ int _atoi(char *s)
         }
         else if (s[i] >= '0' && s[i] <= '9')
         {
+            if (result > limit)
+            {
+                if (sign == 1)
+                    return INT_MAX;
+                else
+                    return INT_MIN;
+            }
             result = result * 10 + (s[i] - '0');
         }
         else if (result > 0)
