1c60ffd26dc93bf5e177b621e6ee166d2d9d9cc2
Kara Jacobs <jacobskara@outlook.com>
2023-02-27 08:36:18 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,47 @@
 #include "main.h"
-#include <stdio.h>
-#include <math.h>
 
 /**
-  * main - Prints the largest prime factor of the number 612852475143
-  *
-  * Return: Always 0 (Success)
-  */
-int main(void)
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the integer value of the string, or 0 if there are no numbers
+ */
+
+int _atoi(char *s)
 {
-	long x, maxf;
-	long number = 612852475143;
-	double square = sqrt(number);
+    int result = 0;
+    int sign = 1;
+    int i = 0;
 
-	for (x = 1; x <= square; x++)
-	{
-		if (number % x == 0)
-		{
-			maxf = number / x;
-		}
-	}
+    /* handle leading whitespace */
+    while (s[i] == ' ')
+    {
+        i++;
+    }
 
-	printf("%ld\n", maxf);
+    /* handle sign */
+    if (s[i] == '-')
+    {
+        sign = -1;
+        i++;
+    }
+    else if (s[i] == '+')
+    {
+        i++;
+    }
 
-	return (0);
+    /* convert string to integer */
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        /* check for overflow */
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10))
+        {
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return result * sign;
 }
