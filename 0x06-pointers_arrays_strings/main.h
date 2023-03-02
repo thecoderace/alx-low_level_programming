a401c692899c97dec83c9fd72aaf0c8ce92f0d3d
Kara Jacobs <jacobskara@outlook.com>
2023-03-01 22:05:09 -0800

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,7 +5,6 @@
 int _putchar(char c);
 
 /* function prototypes */
-
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
@@ -21,5 +20,13 @@ char *string_toupper(char *);
 char *cap_string(char *);
 
 char *leet(char *);
+
+char *rot13(char *);
+
+void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
 
 #endif /* MAIN_H */
